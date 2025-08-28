43050c1a4c867964c136e2b899fc6e48dd38a65f
Leonardo Güths <leonardoguths@gmail.com>
2025-08-28 09:34:19 -0300

    arquivos gerados no vídeo https://www.youtube.com/watch?v=3sQITRihW_A

---
+++ src/routes/Home.tsx
@@ -0,0 +1,45 @@
+import Search from '../components/Search.tsx';
+import { useState } from 'react';
+import type { UserProps } from '../types/user.ts';
+import User from '../components/User.tsx';
+import Error from '../components/Error.tsx';
+
+const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
+
+  const loadUser = async (userName: string) => {
+    setError(false);
+    setUser(null);
+
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await response.json();
+
+    if (response.status === 404) {
+      setError(true);
+      return;
+    }
+
+    const { avatar_url, login, location, followers, following } = data;
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+
+    setUser(userData);
+  };
+
+  return (
+    <div>
+      <Search loadUser={loadUser} />
+      {user && <User {...user} />}
+      {error && <Error />}
+    </div>
+  );
+};
+
+export default Home;
